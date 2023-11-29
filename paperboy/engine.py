61c51d09bb206eaf368b47a3df3d4a6f9ad869ab
Franck Lourme <coton@42.fr>
2023-11-29 17:38:48 +0100

    fix(engine): seek partitions on single mode when out of bulk mode on no_commit

--- paperboy/engine.py
+++ paperboy/engine.py
@@ -30,13 +30,14 @@ class Engine:
 
     handlers: list[type["BaseHandler"]] = []
     steps: list[list[type["BaseHandler"]]] = []
+    __loop: asyncio.AbstractEventLoop
 
     @final
     def __init__(
         self,
         bulk_mode_timeout_ms: int = 10000,
         bulk_mode_max_records: int = 10000,
-        bulk_mode_threshold: int | None = 1000,
+        bulk_mode_threshold: int | None = 0,
         commit_interval_sec: int = 10,
         fail_on_exception: bool = False,
         logger_level: int = logging.INFO,
@@ -56,6 +57,8 @@ class Engine:
             log_level=logger_level,
             with_aiokafka_logs=with_aiokafka_logs,
         )
+
+        self.offsets = {}
 
         self.topic_handlers = {handler.topic: handler for handler in self.handlers}
         self.log.debug(f"topic handlers: {self.topic_handlers}")
@@ -106,15 +109,13 @@ class Engine:
         self.log.addHandler(ch)
 
     @final
-    async def __bulk_mode(self, handlers: list[type["BaseHandler"]] = handlers):
-        loop = asyncio.get_running_loop()
+    async def __bulk_mode(self, handlers: list[type["BaseHandler"]] | None = None):
         in_bulk = True
-        topics_in_bulk = {handler.topic: True for handler in handlers}
+        topics_in_bulk = {handler.topic: True for handler in handlers or self.handlers}
 
         self.consumer.subscribe([topic for topic in topics_in_bulk.keys()])
 
         while in_bulk:
-            offsets = {}
             tasks = []
             messages = await self.consumer.getmany(
                 timeout_ms=self.bulk_mode_timeout_ms,
@@ -124,28 +125,26 @@ class Engine:
             if not messages:
                 break
 
-            self.log.info(
-                f"Received messages:{str({tp: len(msgs) for tp, msgs in messages.items()})}",
-            )
+            self.log.info(f"Received messages:{str({tp: len(msgs) for tp, msgs in messages.items()})}")
             for tp, msgs in messages.items():
                 if tp.topic not in topics_in_bulk.keys():
                     topics_in_bulk[tp.topic] = True
 
                 handler = self.topic_handlers[tp.topic]
-                tasks.append(loop.create_task(handler.handle(msgs)))
+                tasks.append(self.__loop.create_task(handler.handle(msgs)))
 
                 if self.bulk_mode_threshold is not None:
                     highwater = self.consumer.highwater(tp)
-                    if highwater <= msgs[-1].offset + self.bulk_mode_threshold:  # type: ignore
+                    if highwater <= msgs[-1].offset + self.bulk_mode_threshold or 0:  # type: ignore
                         topics_in_bulk[tp.topic] = False
 
-                offsets[tp] = msgs[-1].offset + 1  # type: ignore
+                self.offsets[tp] = msgs[-1].offset + 1  # type: ignore
 
             await asyncio.wait(tasks)
 
-            if not self.consumer._enable_auto_commit and self.with_commit:
-                self.log.info(f"Committing offsets: {offsets}")
-                await self.consumer.commit(offsets)
+            if self.with_commit:
+                self.log.info(f"Committing offsets: {self.offsets}")
+                await self.consumer.commit(self.offsets)
 
             in_bulk = any(topics_in_bulk.values())
 
@@ -154,6 +153,14 @@ class Engine:
     @final
     async def __manual_commit(self):
         self.log.info(f"Starting manual commit loop, with a {self.commit_interval_sec}s interval")
+
+        if not self.with_commit:
+            self.log.warning("with_commit is False, cannot start manual commit task")
+            return
+
+        # To not restart from the beginning when you move from bulk mode to single mode
+        # when the with_commit option is false, we need to set the offsets to the last
+        # read message of each topic.
         while True:
             await asyncio.sleep(self.commit_interval_sec)
             self.log.debug("Committing offsets...")
@@ -163,11 +170,22 @@ class Engine:
     async def __single_mode(self):
         self.log.info("Starting Single Mode")
 
-        if self.with_commit and not self.consumer._enable_auto_commit:
-            loop = asyncio.get_running_loop()
-            loop.create_task(self.__manual_commit())
+        # Assign the same TP as the bulk mode does before, based on the previous subscription
+        if not self.offsets:
+            self.consumer.subscribe([topic for topic in self.topic_handlers.keys()])
+        else:
+            self.consumer.assign([tp for tp in self.offsets.keys()])
 
-        self.consumer.subscribe([topic for topic in self.topic_handlers.keys()])
+        # Create the manual commit task
+        self.__loop.create_task(self.__manual_commit())
+
+        # If the with_commit option is false, we need to set the offsets to the last
+        if self.offsets and not self.with_commit:
+            self.log.info("with_commit is False, setting offsets to the last read message")
+            self.log.debug(f"Offsets: {self.offsets}")
+            for tp, offset in self.offsets.items():
+                self.log.info(f"Seeking topic {tp} to offset {offset}")
+                self.consumer.seek(tp, offset)
 
         async for msg in self.consumer:
             handler = self.topic_handlers[msg.topic]
@@ -202,7 +220,7 @@ class Engine:
 
         Use this method with asyncio.run, or in an asyncio loop.
         """
-        loop = asyncio.get_running_loop()
+        self.__loop = asyncio.get_running_loop()
         try:
             self.consumer = self.configure_consumer()
 
@@ -217,14 +235,14 @@ class Engine:
                 return
             signals = (signal.SIGHUP, signal.SIGTERM, signal.SIGINT)
             for s in signals:
-                loop.add_signal_handler(s, lambda s=s: asyncio.create_task(self.shutdown(s)))
+                self.__loop.add_signal_handler(s, lambda s=s: asyncio.create_task(self.shutdown(s)))
 
-            await loop.create_task(self.__consume())
+            await self.__loop.create_task(self.__consume())
         except (ConsumerStoppedError, asyncio.CancelledError):
             pass
         except Exception as e:
             self.log.exception(e)
-            await self.shutdown(signal.SIGTERM)
+            await self.shutdown()
 
     @final
     async def shutdown(self, signal: signal.Signals | None = None):
